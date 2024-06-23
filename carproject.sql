7b369cb0bc86b8a2fd6a72df4ef882f311524405
Zack <156844269+zeqriamir@users.noreply.github.com>
2024-06-23 05:23:59 -0700

    Add files via upload

---
+++ carproject.sql
@@ -0,0 +1,238 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 16, 2024 at 08:58 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `carproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `ADMIN_ID` varchar(255) NOT NULL,
+  `ADMIN_PASSWORD` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`ADMIN_ID`, `ADMIN_PASSWORD`) VALUES
+('ADMIN', 'ADMIN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `BOOK_ID` int(11) NOT NULL,
+  `CAR_ID` int(11) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `BOOK_PLACE` varchar(255) NOT NULL,
+  `BOOK_DATE` date NOT NULL,
+  `DURATION` int(11) NOT NULL,
+  `PHONE_NUMBER` bigint(20) NOT NULL,
+  `DESTINATION` varchar(255) NOT NULL,
+  `RETURN_DATE` date NOT NULL,
+  `PRICE` int(11) NOT NULL,
+  `BOOK_STATUS` varchar(255) NOT NULL DEFAULT 'UNDER PROCESSING'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `CAR_ID` int(11) NOT NULL,
+  `CAR_NAME` varchar(255) NOT NULL,
+  `FUEL_TYPE` varchar(255) NOT NULL,
+  `CAPACITY` int(11) NOT NULL,
+  `PRICE` int(11) NOT NULL,
+  `CAR_IMG` varchar(255) NOT NULL,
+  `AVAILABLE` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`CAR_ID`, `CAR_NAME`, `FUEL_TYPE`, `CAPACITY`, `PRICE`, `CAR_IMG`, `AVAILABLE`) VALUES
+(1, 'AXIA', 'PETROL', 5, 9, 'Axia.jpg', 'Y'),
+(2, 'MYVI', 'PETROL', 6, 12, 'Myvi.jpg', 'Y'),
+(3, 'BEZZA', 'PETROL', 4, 12, 'Bezza.jpg', 'Y'),
+(4, 'CITY', 'PETROL', 4, 15, 'City.jpg', 'Y'),
+(5, 'CIVIC', 'PETROL', 7, 18, 'Civic.jpg', 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `FED_ID` int(11) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `COMMENT` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `PAY_ID` int(11) NOT NULL,
+  `BOOK_ID` int(11) NOT NULL,
+  `CARD_NO` varchar(255) NOT NULL,
+  `EXP_DATE` varchar(255) NOT NULL,
+  `CVV` int(11) NOT NULL,
+  `PRICE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `FNAME` varchar(255) NOT NULL,
+  `LNAME` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `LIC_NUM` varchar(255) NOT NULL,
+  `PHONE_NUMBER` bigint(11) NOT NULL,
+  `PASSWORD` varchar(255) NOT NULL,
+  `GENDER` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`FNAME`, `LNAME`, `EMAIL`, `LIC_NUM`, `PHONE_NUMBER`, `PASSWORD`, `GENDER`) VALUES
+('Muhd', 'Zeqri', 'muhdzeqri03@gmail.com', '12345678', 114048875, '722531315135f5bcc6044018d0ecf8d8', 'male'),
+('Rash', 'Salim', 'Rashsalim@gmail.com', '34567890', 199827364, 'a3d3565f41e71b0282a626dbb500438c', 'female');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ADMIN_ID`);
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`BOOK_ID`),
+  ADD KEY `CAR_ID` (`CAR_ID`),
+  ADD KEY `EMAIL` (`EMAIL`);
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`CAR_ID`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`FED_ID`),
+  ADD KEY `TEST` (`EMAIL`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`PAY_ID`),
+  ADD UNIQUE KEY `BOOK_ID` (`BOOK_ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`EMAIL`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `BOOK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+
+--
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `CAR_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `FED_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `PAY_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`CAR_ID`) REFERENCES `cars` (`CAR_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`EMAIL`) REFERENCES `users` (`EMAIL`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `TEST` FOREIGN KEY (`EMAIL`) REFERENCES `users` (`EMAIL`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`BOOK_ID`) REFERENCES `booking` (`BOOK_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
